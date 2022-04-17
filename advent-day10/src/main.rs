884391816977d42b44bf3e5344bc96c1e2e17fc7
klishevich <busation@gmail.com>
2022-04-17 08:04:21 +0300

    feat(advent): day 10 part 2

--- advent-day10/src/main.rs
+++ advent-day10/src/main.rs
@@ -6,21 +6,31 @@ fn get_open_brace(c: char) -> char {
         ')' => '(',
         '}' => '{',
         '>' => '<',
-        _ => '?'
+        _ => '?',
     }
 }
 
-fn get_cor_val(c: char) -> i32 {
+fn get_part1_val(c: char) -> i32 {
     match c {
         ']' => 57,
         ')' => 3,
         '}' => 1197,
         '>' => 25137,
-        _ => 0
+        _ => 0,
     }
 }
 
-fn main() {
+fn get_part2_val(c: char) -> i64 {
+    match c {
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
+        _ => 0,
+    }
+}
+
+fn part1() {
     let filename = "src/data-real.txt";
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
@@ -32,7 +42,7 @@ fn main() {
     for line in contents.lines() {
         let mut c_vec: Vec<char> = Vec::new();
         let mut is_corrupted = false;
-        for c in line.chars(){
+        for c in line.chars() {
             if open_braces.contains(&c) {
                 c_vec.push(c);
             } else {
@@ -50,7 +60,55 @@ fn main() {
             println!("- The line is corrupted -");
             println!("{}", line);
         }
-        let res = illegal_char_vec.iter().fold(0, |sum, &c| sum + get_cor_val(c));
+        let res = illegal_char_vec
+            .iter()
+            .fold(0, |sum, &c| sum + get_part1_val(c));
         println!("res {}", res);
     }
+}
+
+fn part2() {
+    let filename = "src/data-real.txt";
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    let open_braces = vec!['(', '{', '[', '<'];
+
+    let mut scores_vec: Vec<i64> = Vec::new();
+
+    for line in contents.lines() {
+        let mut c_vec: Vec<char> = Vec::new();
+        let mut is_corrupted = false;
+        for c in line.chars() {
+            if open_braces.contains(&c) {
+                c_vec.push(c);
+            } else {
+                let index_option = c_vec.iter().rev().position(|&r| r == get_open_brace(c));
+                if index_option == None || index_option.unwrap() != 0 {
+                    is_corrupted = true;
+                    break;
+                } else {
+                    c_vec.remove(c_vec.len() - 1);
+                }
+            }
+        }
+        if is_corrupted {
+            println!("- Corrupted {}", line);
+        } else {
+            let score = c_vec
+                .into_iter()
+                .rev()
+                .fold(0, |acc, c| acc * 5 + get_part2_val(c));
+            scores_vec.push(score);
+        }
+
+        println!("{:?}", scores_vec);
+    }
+
+    scores_vec.sort_unstable();
+
+    println!("Res {}", scores_vec[(scores_vec.len() - 1) / 2]);
+}
+
+fn main() {
+    part2();
 }
