33c0472e9fbc4a399a00c13afb64d8e9c0eb02d6
klishevich <busation@gmail.com>
2022-05-22 18:19:58 +0300

    Update main.rs

--- advent-day12/src/main.rs
+++ advent-day12/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
     let content = fs::read_to_string("src/data-test1.txt").expect("we have a bug");
     for line in content.lines() {
-        // println!("new line");
+        // TODO: remove copy paste
         let (left_str, right_str) = line.split_once("-").unwrap();
         if left_str != "end" && right_str != "start" {
             let routes_to_opt = routes_map.get_mut(left_str);
@@ -30,17 +30,11 @@ fn main() {
         }
     }
 
-    // for (key, value) in &routes_map {
-    //     print!("{} : ", key);
-    //     println!("{:?}", value);
-    // }
-
-    let mut bfs_next_id: i32 = 0;
     let mut res_paths: Vec<Vec<&str>> = Vec::new();
 
     // VEC DEQUE
-    // (id, path, visited, visited_twice)
-    let mut queue: VecDeque<(i32, Vec<&str>, HashMap<&str, i32>, bool)> = VecDeque::new();
+    // (path, visited, visited_twice)
+    let mut queue: VecDeque<(Vec<&str>, HashMap<&str, i32>, bool)> = VecDeque::new();
 
     let start_routes_to = routes_map.get("start").unwrap();
     for el in start_routes_to {
@@ -49,12 +43,11 @@ fn main() {
             visited_hash_map.insert(*el, 1);
         }
         // add element to queue
-        queue.push_back((bfs_next_id, vec!["start", *el], visited_hash_map, false));
-        bfs_next_id += 1;
+        queue.push_back((vec!["start", *el], visited_hash_map, false));
     }
 
     while queue.len() > 0 {
-        let (_cur_id, cur_path_vec, cur_visited_map, cur_visited_twice) = queue.pop_front().unwrap();
+        let (cur_path_vec, cur_visited_map, cur_visited_twice) = queue.pop_front().unwrap();
         let cur_str = cur_path_vec.last().unwrap();
         let routes_to = routes_map.get(cur_str).unwrap();
         for e in routes_to {
@@ -69,18 +62,17 @@ fn main() {
                 cur_path_clone_vec.push(*e);
                 if *e == "end" {
                     res_paths.push(cur_path_clone_vec);
-                } else {
-                    let mut cur_visited_clone_map = cur_visited_map.clone();
-                    if e.chars().next().unwrap().is_lowercase() {
-                        cur_visited_clone_map.insert(*e, visited_times + 1);
-                    }
+                    continue;
+                }
+                let mut cur_visited_clone_map = cur_visited_map.clone();
+                if e.chars().next().unwrap().is_lowercase() {
+                    cur_visited_clone_map.insert(*e, visited_times + 1);
+                }
 
-                    if (visited_times == 1 || cur_visited_twice == true) {
-                        queue.push_back((bfs_next_id, cur_path_clone_vec, cur_visited_clone_map, true));
-                    } else {
-                        queue.push_back((bfs_next_id, cur_path_clone_vec, cur_visited_clone_map, false));
-                    }
-                    bfs_next_id += 1;
+                if visited_times == 1 || cur_visited_twice == true {
+                    queue.push_back((cur_path_clone_vec, cur_visited_clone_map, true));
+                } else {
+                    queue.push_back((cur_path_clone_vec, cur_visited_clone_map, false));
                 }
             }
         }
