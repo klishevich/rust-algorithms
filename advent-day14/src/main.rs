b0a39918c88bb734412275ec3ce7f47788336fe9
klishevich <busation@gmail.com>
2022-06-25 20:09:02 +0300

    day 14 part 2 calc rule results 10

--- advent-day14/src/main.rs
+++ advent-day14/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::collections::HashMap;
 
-fn main() {
+fn part1() {
     let content = fs::read_to_string("src/data-test1.txt").expect("we have a bug");
 
     let initial_line = content.lines().next().unwrap();
@@ -70,4 +70,87 @@ fn main() {
     }
 
     println!("res {}", max - min);
+}
+
+fn part2() {
+    let content = fs::read_to_string("src/data-test1.txt").expect("we have a bug");
+
+    // INITIAL POLYMER
+    let initial_line = content.lines().next().unwrap();
+    println!("initial_line {}", &initial_line);
+
+    // RULES
+    let mut rules_map: HashMap<char, HashMap<char, char>> = HashMap::new();
+    for (index, line) in content.lines().enumerate() {
+        if index < 2 {
+            continue;
+        }
+        let (left, right) = line.split_once(" -> ").unwrap();
+        let char1 = left.chars().next().unwrap();
+        let char2 = left.chars().nth(1).unwrap();
+        let char3 = right.chars().next().unwrap();
+        rules_map.entry(char1).or_default().insert(char2, char3);
+    }
+    println!("rules_map");
+    for (ch1, map1) in &rules_map {
+        println!("{}", ch1);
+        for (ch2, ch3) in map1 {
+            println!(" {} -> {}", ch2, ch3);
+        }
+    }
+
+    // CALC RULE_RESULTS
+    let mut rule_results10: HashMap<char, HashMap<char, HashMap<char, i64>>> = HashMap::new();
+    for (ch1, map1) in &rules_map {
+        println!("{}", ch1);
+        for (ch2, _) in map1 {
+            let mut char_vec: Vec<char> = vec![*ch1, *ch2];
+            let steps = 10;
+            for _j in 0..steps {
+                let mut i = 0;
+                let mut len = char_vec.len();
+                loop {
+                    let el = char_vec.get(i).unwrap();
+                    let next_el = char_vec.get(i+1).unwrap();
+                    match rules_map.get(el) {
+                        Some(char_map1) => {
+                            match char_map1.get(next_el) {
+                                Some(ins_char) => {
+                                    char_vec.insert(i+1, *ins_char);
+                                    len = len + 1;
+                                    i = i + 2;
+                                },
+                                None => i = i + 1
+                            };
+                        },
+                        None => i = i + 1
+                    };
+                    if i >= len - 1 {
+                        break;
+                    };
+                }
+            }
+            let mut count_map: HashMap<char, i64> = HashMap::new();
+            for ch in char_vec {
+                // MAP ENTRY OR INSERT
+                *count_map.entry(ch).or_insert(0) += 1;
+            }
+            rule_results10.entry(*ch1).or_default().insert(*ch2, count_map);
+        }
+    }
+
+    println!("rule_results10");
+    for (ch1, map1) in &rule_results10 {
+        println!("{}", ch1);
+        for (ch2, map2) in map1 {
+            println!(" {}", ch2);
+            for (ch3, cnt) in map2 {
+                println!("  {} -> {}", ch3, cnt);
+            }
+        }
+    }
+}
+
+fn main() {
+    part2();
 }
