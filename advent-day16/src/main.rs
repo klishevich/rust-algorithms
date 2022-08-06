aae6b8fe7593bcfbd07ddeb11b2d689f8264c4e6
klishevich <busation@gmail.com>
2022-08-06 10:35:59 +0300

    day 16 refactor to use byte string

--- advent-day16/src/main.rs
+++ advent-day16/src/main.rs
@@ -2,20 +2,19 @@ use std::fs;
 use std::i64;
 use std::str;
 
-struct PacketParser {
+struct PParser {
     ver_sum: u32,
     depth: u32,
 }
 
 // RECURSION
-impl PacketParser {
+impl PParser {
     // REFERENCE TO MUTABLE INT
     fn read_int(bytes_str: &[u8], start: &mut usize, chars: usize) -> u32 {
         let end = *start + chars;
         let str = str::from_utf8(&bytes_str[*start..end]).unwrap();
-        let val = u32::from_str_radix(str, 2).unwrap();
         *start += chars;
-        return val;
+        return u32::from_str_radix(str, 2).unwrap();
     }
 
     fn read_move(start: &mut usize, chars: usize) {
@@ -31,6 +30,9 @@ impl PacketParser {
     // }
 
     fn update_res(acc_val: u64, new_val: u64, p_type: u32) -> u64 {
+        if acc_val == u64::MAX {
+            return new_val;
+        }
         match p_type {
             0 => acc_val + new_val,
             1 => acc_val * new_val,
@@ -73,66 +75,56 @@ impl PacketParser {
         }
     }
 
-    pub fn run(&mut self, s: &str) -> (usize, u64) {
+    pub fn run(&mut self, bs: &[u8], bs_start: usize) -> (usize, u64) {
         self.depth += 1;
         println!("-- PacketParser depth {} --", self.depth);
-        let mut pos: usize = 0;
-        let bs = s.as_bytes();
-        let p_ver = PacketParser::read_int(bs, &mut pos, 3);
+        let mut pos: usize = bs_start;
+        let p_ver = PParser::read_int(bs, &mut pos, 3);
         self.ver_sum += p_ver;
-        let p_type = PacketParser::read_int(bs, &mut pos, 3);
+        let p_type = PParser::read_int(bs, &mut pos, 3);
 
         if p_type == 4 {
             let mut accum_val: String = "".to_string();
             loop {
-                let first_bit = PacketParser::read_int(bs, &mut pos, 1);
+                let first_bit = PParser::read_int(bs, &mut pos, 1);
                 accum_val += str::from_utf8(&bs[pos..pos + 4]).unwrap();
-                PacketParser::read_move(&mut pos, 4);
+                PParser::read_move(&mut pos, 4);
                 if first_bit == 0 {
                     break;
                 }
             }
-
             let val = u64::from_str_radix(&accum_val, 2).unwrap();
             self.depth -= 1;
             return (pos, val);
         } else {
-            let length_id = PacketParser::read_int(bs, &mut pos, 1);
+            let length_id = PParser::read_int(bs, &mut pos, 1);
             if length_id == 0 {
                 let bits_in_sub_packets: usize =
-                    PacketParser::read_int(bs, &mut pos, 15).try_into().unwrap();
+                    PParser::read_int(bs, &mut pos, 15).try_into().unwrap();
                 let end_pos = pos + bits_in_sub_packets;
-                let mut end_acc_val = u64::MAX;
+                let mut cur_val = u64::MAX;
+                let mut prev_val: u64;
                 loop {
-                    let sub_str = str::from_utf8(&bs[pos..s.len()]).unwrap();
-                    let (r_pos, r_val) = PacketParser::run(self, sub_str);
-                    pos = pos + r_pos;
-                    if end_acc_val == u64::MAX {
-                        end_acc_val = r_val
-                    } else {
-                        end_acc_val = PacketParser::update_res(end_acc_val, r_val, p_type);
-                    }
+                    prev_val = cur_val;
+                    (pos, cur_val) = PParser::run(self, bs, pos);
+                    cur_val = PParser::update_res(prev_val, cur_val, p_type);
                     if pos >= end_pos {
                         break;
                     }
                 }
                 self.depth -= 1;
-                return (end_pos, end_acc_val);
+                return (end_pos, cur_val);
             } else {
-                let number_of_sub_packets = PacketParser::read_int(bs, &mut pos, 11);
-                let mut end_acc_val = u64::MAX;
+                let number_of_sub_packets = PParser::read_int(bs, &mut pos, 11);
+                let mut cur_val = u64::MAX;
+                let mut prev_val: u64;
                 for _ in 0..number_of_sub_packets {
-                    let sub_str = str::from_utf8(&bs[pos..s.len()]).unwrap();
-                    let (r_pos, r_val) = PacketParser::run(self, sub_str);
-                    pos = pos + r_pos;
-                    if end_acc_val == u64::MAX {
-                        end_acc_val = r_val
-                    } else {
-                        end_acc_val = PacketParser::update_res(end_acc_val, r_val, p_type);
-                    }
+                    prev_val = cur_val;
+                    (pos, cur_val) = PParser::run(self, bs, pos);
+                    cur_val = PParser::update_res(prev_val, cur_val, p_type);
                 }
                 self.depth -= 1;
-                return (pos, end_acc_val);
+                return (pos, cur_val);
             }
         }
     }
@@ -141,7 +133,6 @@ impl PacketParser {
 fn task(file_name: &str) -> (u32, u64) {
     let content = fs::read_to_string(file_name).expect("reading file error");
     let str = content.lines().next().unwrap();
-    println!("str {}", str);
     // maybe string of bytes b""?
     let mut b_str = "".to_string();
     for c in str.chars() {
@@ -156,11 +147,12 @@ fn task(file_name: &str) -> (u32, u64) {
         b_str += &b_str2;
     }
 
-    let mut parser = PacketParser {
+    let mut parser = PParser {
         ver_sum: 0,
         depth: 0,
     };
-    let (_, end_val) = parser.run(&b_str);
+    let bs = b_str.as_bytes();
+    let (_, end_val) = parser.run(bs, 0);
     println!("ver_sum {}", parser.ver_sum);
     println!("end_val {}", end_val);
     return (parser.ver_sum, end_val);
