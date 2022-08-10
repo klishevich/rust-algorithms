50a144fef054180ddf406c43c11fc267e9e0aab4
klishevich <busation@gmail.com>
2022-08-10 10:51:02 +0300

    day 17 get x_max

--- advent-day17/src/main.rs
+++ advent-day17/src/main.rs
@@ -4,7 +4,19 @@ fn task(x1: i32, x2: i32, y1: i32, y2: i32) -> i32 {
 }
 
 fn main() {
+    let get_x_max_fn = |v_x: i32| -> i32 { 
+        let mut x_max = 0;
+        for i in 0..v_x + 1 {
+            x_max += v_x - i;
+        }
+        return x_max;
+    };
+
     let res = task(20, 30, -10, -5);
+    let v_x0 = 7;
+    let v_y0 = 2;
+
+    println!("x_max {}", get_x_max_fn(v_x0));
     println!("res {}", res);
 }
 
