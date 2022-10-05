6f1533834022188c763dd6acc6e15c849e64b806
klishevich <busation@gmail.com>
2022-10-05 10:00:17 +0300

    day 19 add get_permuted_point function

--- advent-day19/src/main.rs
+++ advent-day19/src/main.rs
@@ -1,10 +1,32 @@
-use std::fs;
+use itertools::Itertools;
 use std::collections::HashMap;
+use std::fs;
+
+type Point = (i32, i32, i32);
+
+/**
+ * Permutation value could be from 0 to 47
+ */
+fn get_permuted_point(p: Point, permutation: u8) -> Point {
+    let p1: u8 = permutation % 6;
+    println!("p1 {}", p1);
+    let r1: u8 = permutation / 6;
+    let p2: u8 = r1 % 4;
+    println!("p2 {}", p2);
+    let p3: u8 = r1 / 4;
+    println!("p3 {}", p3);
+    let mut x1: u8;
+    let mut y1: u8;
+    let mut z1: u8;
+
+    let (x, y, z) = p;
+    return (x, y, z);
+}
 
 fn main() {
     let content = fs::read_to_string("src/data01.txt").expect("some bug");
     let mut scanner_id: i32 = -1;
-    let mut scanner_map: HashMap<i32, Vec<(i32,i32)>> = HashMap::new();
+    let mut scanner_map: HashMap<i32, Vec<Point>> = HashMap::new();
     for (index, line) in content.lines().enumerate() {
         println!("index {}, line {}", index, line);
         if line.contains("scanner") {
@@ -13,15 +35,23 @@ fn main() {
         } else if line.is_empty() {
             continue;
         } else {
-            let (left_str, right_str) = line.split_once(",").unwrap();
-            let left_i32: i32 = left_str.parse().unwrap();
-            let right_i32: i32 = right_str.parse().unwrap();
-            scanner_map.get_mut(&scanner_id).unwrap().push((left_i32, right_i32));
+            let strs: Vec<&str> = line.split(",").collect();
+            let first: i32 = strs[0].parse().unwrap();
+            let second: i32 = strs[1].parse().unwrap();
+            let third: i32 = strs[2].parse().unwrap();
+            scanner_map
+                .get_mut(&scanner_id)
+                .unwrap()
+                .push((first, second, third));
         }
     }
 
-    for (k,v) in scanner_map {
-        println!("key {}", k);
-        println!("{:?}", v);
-    }
+    // MAP SORT HASHMAP
+    // for key in scanner_map.keys().sorted() {
+    //     println!("{}", key);
+    //     println!("{:?}", scanner_map[key]);
+    // }
+
+    let r = get_permuted_point((1,2,3), 47);
+    println!("{:?}", r);
 }
