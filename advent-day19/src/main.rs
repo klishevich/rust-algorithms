ecdb79bb3a70166bbcbb5f01bed99aaf96407921
klishevich <busation@gmail.com>
2022-10-08 10:57:40 +0300

    day 19 first now working algorithm

--- advent-day19/src/main.rs
+++ advent-day19/src/main.rs
@@ -1,10 +1,21 @@
 use itertools::Itertools;
+use core::num;
 use std::collections::HashMap;
 use std::fs;
 
 type Point = (i32, i32, i32);
 
-fn get_permuted_point(p: Point, change_sign_x: bool, change_sign_y: bool, change_sign_z: bool, swap_yz: bool, shift_all: u8) -> Point {
+/**
+ * The shift_all should accept one of the following value 0,1,2
+ */
+fn get_permuted_point(
+    p: Point,
+    change_sign_x: bool,
+    change_sign_y: bool,
+    change_sign_z: bool,
+    swap_yz: bool,
+    shift_all: u8,
+) -> Point {
     let (mut x, mut y, mut z) = p;
     if change_sign_x {
         x = -x;
@@ -35,12 +46,43 @@ fn get_permuted_point(p: Point, change_sign_x: bool, change_sign_y: bool, change
     return (x, y, z);
 }
 
+/**
+ * The num should accept value within range [0, 47]
+ */
+fn decompose_number(num: u8) -> (bool, bool, bool, bool, u8) {
+    let change_sign_x = num % 2 == 1;
+    let mut residue = num / 2;
+    let change_sign_y = residue % 2 == 1;
+    residue = residue / 2;
+    let change_sign_z = residue % 2 == 1;
+    residue = residue / 2;
+    let swap_yz = residue % 2 == 1;
+    let shift_all = residue / 2;
+    return (
+        change_sign_x,
+        change_sign_y,
+        change_sign_z,
+        swap_yz,
+        shift_all,
+    );
+}
+
+fn point_to_origin(p: Point, origin: Point) -> Point {
+    return (p.0 - origin.0, p.1 - origin.1, p.2 - origin.2);
+}
+
+fn points_equal(p1: Point, p2: Point) -> bool {
+    if p1.0 == p2.0 && p1.1 == p2.1 && p1.2 == p2.2 {
+        return true;
+    }
+    return false;
+}
+
 fn main() {
     let content = fs::read_to_string("src/data01.txt").expect("some bug");
     let mut scanner_id: i32 = -1;
     let mut scanner_map: HashMap<i32, Vec<Point>> = HashMap::new();
     for (index, line) in content.lines().enumerate() {
-        println!("index {}, line {}", index, line);
         if line.contains("scanner") {
             scanner_id += 1;
             scanner_map.insert(scanner_id, Vec::new());
@@ -59,27 +101,84 @@ fn main() {
     }
 
     // MAP SORT HASHMAP
-    // for key in scanner_map.keys().sorted() {
-    //     println!("{}", key);
-    //     println!("{:?}", scanner_map[key]);
-    // }
+    for key in scanner_map.keys().sorted() {
+        println!("{}", key);
+        println!("{:?}", scanner_map[key]);
+    }
 
-    let r = get_permuted_point((1,2,3), true, false, false, false, 1);
-    println!("{:?}", r);
+    for key1 in scanner_map.keys().sorted() {
+        for key2 in scanner_map.keys().sorted() {
+            if *key1 < *key2 {
+                println!("key1 {}, key2 {}", key1, key2);
+                let beacon_list1 = scanner_map.get(key1).unwrap();
+                let beacon_list2 = scanner_map.get(key2).unwrap();
+                for beacon_list1_origin in beacon_list1 {
+                    for beacon_list2_origin in beacon_list2 {
+                        for permutation in 0..48 {
+                            let (change_sign_x, change_sign_y, change_sign_z, swap_yz, shift_all) =
+                                decompose_number(permutation);
+                            let mut number_of_matches = 0;
+                            for beacon1 in beacon_list1 {
+                                for beacon2 in beacon_list2 {
+                                    let beacon1_new_origin = point_to_origin(*beacon1, *beacon_list1_origin);
+                                    let beacon2_new_origin = point_to_origin(*beacon2, *beacon_list1_origin);
+                                    let beacon2_new_origin_perm = get_permuted_point(
+                                        beacon2_new_origin,
+                                        change_sign_x,
+                                        change_sign_y,
+                                        change_sign_z,
+                                        swap_yz,
+                                        shift_all,
+                                    );
+                                    if points_equal(beacon1_new_origin, beacon2_new_origin_perm) {
+                                        number_of_matches += 1;
+                                    }
+                                }
+                            }
+                            if number_of_matches >= 12 {
+                                println!("!!! number_of_matches {}, beacon_list1_origin {:?}, beacon_list2_origin {:?}", number_of_matches, beacon_list1_origin, beacon_list2_origin);
+                                println!("change_sign_x {}, change_sign_y {}, change_sign_z {}, swap_yz {}, shift_all {}", change_sign_x,
+                                change_sign_y,
+                                change_sign_z,
+                                swap_yz,
+                                shift_all);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // let r = get_permuted_point((1,2,3), true, false, false, false, 1);
+    // println!("{:?}", r);
 }
 
 mod test {
+    use crate::decompose_number;
     use crate::get_permuted_point;
 
     #[test]
     fn get_permuted_point_test() {
-        let res = get_permuted_point((1,2,3), true, false, false, false, 1);
+        let res = get_permuted_point((1, 2, 3), true, false, false, false, 1);
         assert_eq!(res, (3, -1, 2));
     }
 
     #[test]
     fn get_permuted_point_test1() {
-        let res = get_permuted_point((1,2,3), false, false, false, false, 2);
+        let res = get_permuted_point((1, 2, 3), false, false, false, false, 2);
         assert_eq!(res, (2, 3, 1));
+    }
+
+    #[test]
+    fn decompose_number_test1() {
+        let res = decompose_number(0);
+        assert_eq!(res, (false, false, false, false, 0));
+    }
+
+    #[test]
+    fn decompose_number_test2() {
+        let res = decompose_number(47);
+        assert_eq!(res, (true, true, true, true, 2));
     }
 }
