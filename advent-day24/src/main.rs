41a759c7e1b097f3c38df0b0a684108756e7d8b1
klishevich <busation@gmail.com>
2022-11-30 10:11:03 +0300

    day 24 still no solution

--- advent-day24/src/main.rs
+++ advent-day24/src/main.rs
@@ -2,17 +2,17 @@ use std::collections::HashMap;
 use std::fs;
 
 const UNDEF_VAR: usize = 666;
-const UNDEF_I32: i32 = 777;
+const UNDEF_I: i64 = 777;
 
 // bool is var
-type TCommandTypeVar1Var2Val2 = (u8, usize, usize, i32);
+type TCommandTypeVar1Var2Val2 = (u8, usize, usize, i64);
 
-fn parse_var(s: &str) -> (usize, i32) {
+fn parse_var(s: &str) -> (usize, i64) {
     match s {
-        "w" => (0, UNDEF_I32),
-        "x" => (1, UNDEF_I32),
-        "y" => (2, UNDEF_I32),
-        "z" => (3, UNDEF_I32),
+        "w" => (0, UNDEF_I),
+        "x" => (1, UNDEF_I),
+        "y" => (2, UNDEF_I),
+        "z" => (3, UNDEF_I),
         other => (UNDEF_VAR, other.parse().unwrap()),
     }
 }
@@ -37,7 +37,7 @@ fn read_alu_commands_from_file(file_name: &str) -> Vec<TCommandTypeVar1Var2Val2>
         };
         let (var_a, _val_a) = parse_var(elements.next().unwrap());
         let mut var_b: usize = UNDEF_VAR;
-        let mut val_b: i32 = UNDEF_I32;
+        let mut val_b: i64 = UNDEF_I;
         if !elements.peek().is_none() {
             (var_b, val_b) = parse_var(elements.next().unwrap());
         }
@@ -46,14 +46,14 @@ fn read_alu_commands_from_file(file_name: &str) -> Vec<TCommandTypeVar1Var2Val2>
     return res;
 }
 
-fn get_b_val(var_vec: &Vec<i32>, var_b: &usize, val_b: &i32) -> i32 {
+fn get_b_val(var_vec: &Vec<i64>, var_b: &usize, val_b: &i64) -> i64 {
     if *var_b == UNDEF_VAR {
         return *val_b;
     }
     return var_vec[*var_b];
 }
 
-fn exec_command(var_vec: &mut Vec<i32>, cmd: &TCommandTypeVar1Var2Val2, inp: i32) -> () {
+fn exec_command(var_vec: &mut Vec<i64>, cmd: &TCommandTypeVar1Var2Val2, inp: i64) -> () {
     let (cmd_type, var_a, var_b, val_b) = cmd;
     match *cmd_type {
         1 => {
@@ -84,8 +84,8 @@ fn exec_command(var_vec: &mut Vec<i32>, cmd: &TCommandTypeVar1Var2Val2, inp: i32
 }
 
 fn exec_all_commands(
-    var_vec: &mut Vec<i32>,
-    inp_vec: &Vec<i32>,
+    var_vec: &mut Vec<i64>,
+    inp_vec: &Vec<i64>,
     commands: &Vec<TCommandTypeVar1Var2Val2>,
 ) {
     let mut inp_idx = 0;
@@ -101,7 +101,7 @@ fn exec_all_commands(
 }
 
 fn exec_all_commands2(
-    var_vec: &mut Vec<i32>,
+    var_vec: &mut Vec<i64>,
     inp_num: u64,
     commands: &Vec<TCommandTypeVar1Var2Val2>,
 ) {
@@ -111,7 +111,7 @@ fn exec_all_commands2(
     for cmd in commands {
         if cmd.0 == 1 {
             let cc = inp_chars_iter.next().unwrap();
-            let inp: i32 = cc.to_digit(10).unwrap().try_into().unwrap();
+            let inp: i64 = cc.to_digit(10).unwrap().try_into().unwrap();
             exec_command(var_vec, cmd, inp);
         } else {
             exec_command(var_vec, cmd, 0);
@@ -119,15 +119,15 @@ fn exec_all_commands2(
     }
 }
 
-fn get_vectors(digit: u8, other: i32) -> Vec<Vec<i32>> {
-    let mut res: Vec<Vec<i32>> = Vec::new();
+fn get_vectors(digit: u8, base: &Vec<i64>) -> Vec<Vec<i64>> {
+    let mut res: Vec<Vec<i64>> = Vec::new();
     for i in 1..10 {
-        let mut v: Vec<i32> = Vec::new();
+        let mut v: Vec<i64> = Vec::new();
         for j in 1..15 {
             if j == digit {
                 v.push(i);
             } else {
-                v.push(other);
+                v.push(base[usize::try_from(j - 1).unwrap()]);
             }
         }
         res.push(v);
@@ -140,7 +140,7 @@ fn main() {
     let max: u64 = 99999999999999;
     // for i in 0..10 {
     //     let j = max - i;
-    //     let mut var_vec: Vec<i32> = vec![0; 4];
+    //     let mut var_vec: Vec<i64> = vec![0; 4];
     //     exec_all_commands2(&mut var_vec, j, &commands);
     //     if var_vec[3] == 1 {
     //         println!("res {}", j);
@@ -153,23 +153,28 @@ fn main() {
 
     // println!("-- 1 --");
     // for i in 11111111111111..91111111111111 {
-    //     let mut var_vec: Vec<i32> = vec![0; 4];
+    //     let mut var_vec: Vec<i64> = vec![0; 4];
     //     exec_all_commands2(&mut var_vec, i, &commands);
     //     println!("res {}", var_vec[3]);
     // }
 
-    let oth = 9;
-    println!("oth {}", oth);
-    for k in 1..15 {
-        println!("k {}", k);
-        let vv = get_vectors(k, oth);
-        // for v in &vv {
-        //     println!("{:?}", v);
-        // }
-        for (i_v, v) in vv.iter().enumerate() {
-            let mut var_vec: Vec<i32> = vec![0; 4];
-            exec_all_commands(&mut var_vec, &v, &commands);
-            println!("res {}: {}", i_v, var_vec[3]);
+    
+    for pp in 1..2 {
+        println!("---- pp {} -----", pp);
+        // let base:     Vec<i64> = v[1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14];
+        let mut base: Vec<i64> = vec![1, 1, 9, 1, 3, 8, 9, 9, 9, 7, 9, 5, 1, 1];
+        // base[7]=pp;
+        for k in 1..15 {
+            println!("k {}", k);
+            let vv = get_vectors(k, &base);
+            // for v in &vv {
+            //     println!("{:?}", v);
+            // }
+            for (i_v, v) in vv.iter().enumerate() {
+                let mut var_vec: Vec<i64> = vec![0; 4];
+                exec_all_commands(&mut var_vec, &v, &commands);
+                println!("res {}: {}", i_v + 1, var_vec[3]);
+            }
         }
     }
 }
@@ -181,8 +186,8 @@ mod test {
 
     #[test]
     fn test1() {
-        let mut var_vec: Vec<i32> = vec![0; 4];
-        let mut inp: Vec<i32> = vec![4];
+        let mut var_vec: Vec<i64> = vec![0; 4];
+        let mut inp: Vec<i64> = vec![4];
         let commands: Vec<TCommandTypeVar1Var2Val2> =
             vec![(1, 0, UNDEF_VAR, 0), (3, 0, UNDEF_VAR, -1)];
         exec_all_commands(&mut var_vec, &mut inp, &commands);
@@ -191,8 +196,8 @@ mod test {
 
     #[test]
     fn test2() {
-        let mut var_vec: Vec<i32> = vec![0; 4];
-        let mut inp: Vec<i32> = vec![1, 3];
+        let mut var_vec: Vec<i64> = vec![0; 4];
+        let mut inp: Vec<i64> = vec![1, 3];
         let commands: Vec<TCommandTypeVar1Var2Val2> = vec![
             (1, 3, UNDEF_VAR, 0),
             (1, 1, UNDEF_VAR, 0),
@@ -205,8 +210,8 @@ mod test {
 
     #[test]
     fn test2_1() {
-        let mut var_vec: Vec<i32> = vec![0; 4];
-        let mut inp: Vec<i32> = vec![3, 1];
+        let mut var_vec: Vec<i64> = vec![0; 4];
+        let mut inp: Vec<i64> = vec![3, 1];
         let commands: Vec<TCommandTypeVar1Var2Val2> = vec![
             (1, 3, UNDEF_VAR, 0),
             (1, 1, UNDEF_VAR, 0),
@@ -219,8 +224,8 @@ mod test {
 
     #[test]
     fn test3() {
-        let mut var_vec: Vec<i32> = vec![0; 4];
-        let mut inp: Vec<i32> = vec![15];
+        let mut var_vec: Vec<i64> = vec![0; 4];
+        let mut inp: Vec<i64> = vec![15];
         let commands: Vec<TCommandTypeVar1Var2Val2> = vec![
             (1, 0, UNDEF_VAR, 0),
             (2, 3, 0, 0),
